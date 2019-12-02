8e06af81e654364e904595617e8abcd4a3b7f1df
MinexAutomation <minex.automation@gmail.com>
2019-12-02 11:10:04 -0800

    Moved endpoint and endpoint factory from R5T.Sundsvall.Base.

---
+++ source/R5T.Epidarus.Base/Code/Interfaces/IEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using R5T.Sindia;
+
+
+namespace R5T.Epidarus
+{
+    public interface IEndpoint
+    {
+        /// <summary>
+        /// Sends an anomaly to the endpoint.
+        /// </summary>
+        void Send(AnomalyIdentity anomaly);
+
+        /// <summary>
+        /// Used in debugging, or to exactly specify the message to send to an endpoint.
+        /// </summary>
+        void Send(string message);
+    }
+}
