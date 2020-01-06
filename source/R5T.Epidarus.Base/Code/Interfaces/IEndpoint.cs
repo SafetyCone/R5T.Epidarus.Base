9726dcf27014306e33526f1c11c2b675de5a36cb
MinexAutomation <minex.automation@gmail.com>
2020-01-05 20:56:10 -0800

    Changed endpoint interface type and endpoint factory to support asynchronous sending.

--- source/R5T.Epidarus.Base/Code/Interfaces/IEndpoint.cs
+++ source/R5T.Epidarus.Base/Code/Interfaces/IEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 using R5T.Sindia;
 
@@ -8,13 +9,25 @@ namespace R5T.Epidarus
     public interface IEndpoint
     {
         /// <summary>
-        /// Sends an anomaly to the endpoint.
-        /// </summary>
-        void Send(AnomalyIdentity anomaly);
-
-        /// <summary>
         /// Used in debugging, or to exactly specify the message to send to an endpoint.
         /// </summary>
         void Send(string message);
+
+        /// <summary>
+        /// Used in debugging, or to exactly specify the message to send to an endpoint.
+        /// Asynchronous.
+        /// </summary>
+        Task SendAsync(string message);
+
+        /// <summary>
+        /// Sends an anomaly to the endpoint, allowing the endpoint to determine the message sent to the anomaly.
+        /// </summary>
+        void Send(AnomalyIdentity anomalyIdentity);
+
+        /// <summary>
+        /// Sends an anomaly to the endpoint, allowing the endpoint to determine the message sent to the anomaly.
+        /// Asynchronous.
+        /// </summary>
+        Task SendAsync(AnomalyIdentity anomalyIdentity);
     }
 }
