439521788704975e5d88c9fd0f65146856579aa0
davidcoats <david@getsafetycone.com>
2022-04-27 15:30:53 -0700

    Interim changes.

---
+++ source/R5T.Epidarus.Base/Code/Services/Definitions/IEndpointFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using R5T.Endalia;
+
+using R5T.T0064;
+
+
+namespace R5T.Epidarus
+{
+    [ServiceDefinitionMarker]
+    public interface IEndpointFactory : IServiceDefinition
+    {
+        Task<List<IEndpoint>> GetEndpointsAsync(IEnumerable<EndpointIdentity> endpointIdentities);
+    }
+}
